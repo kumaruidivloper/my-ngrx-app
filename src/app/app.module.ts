51c61b926cc6bd4cca0705a9ed733e26e96fc65a
kumaravel shanmugam <kumaruidivloper@gmail.com>
2017-11-26 12:24:45 +0530

    Simple Ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    StoreModule.forRoot({message: simpleReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
